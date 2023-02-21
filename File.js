12d04467718329d644d303dcaffd24db9eef1702
Shahryar <56665325+Shar-new@users.noreply.github.com>
2023-02-21 14:01:31 -0800

    Fix javascript link to html

--- File.js
+++ File.js
@@ -2,3 +2,8 @@ let guess = prompt("Enter either rock, paper or scissor");
 let computerAnswer = ["rock","paper", "scissor"];
 console.log(computerAnswer);
 
+function getComputerChoice (x,y) {
+    Math.floor(Math.random()* 4);
+
+    return 
+}
