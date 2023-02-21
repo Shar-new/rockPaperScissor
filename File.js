edba26222fb77531b274ed98c80c0e58187a6646
Shahryar <56665325+Shar-new@users.noreply.github.com>
2023-02-21 14:19:22 -0800

    Add result function for game

--- File.js
+++ File.js
@@ -2,8 +2,35 @@ let guess = prompt("Enter either rock, paper or scissor");
 let computerAnswer = ["rock","paper", "scissor"];
 console.log(computerAnswer);
 
-function getComputerChoice (x,y) {
-    Math.floor(Math.random()* 4);
-
-    return 
+function getComputerChoice () {
+    index = Math.floor(Math.random()* 3);
+    x = computerAnswer[index];
+    return x;
 }
+console.log(getComputerChoice())
+
+function result (x,y) {
+    if (x == y) {
+
+        console.log("tie");
+
+    } else if (x == "rock" && y=="paper"){
+        console.log("you win!!!");
+
+    } else if (x == "paper" && y == "rock") {
+        console.log("You lost");
+
+    } else if (x == "paper" && y== "scissor"){
+        console.log("You win");
+
+    } else if (x == "scissor" && y == "rock") {
+        console.log("you Win");
+
+    } else if (x == "scissor" && y == "paper") {
+        console.log("you lose");
+    } else if (x == "rock" && y == "scissor") {
+        console.log("You lose!")
+    }
+}
+
+result(x, guess);
